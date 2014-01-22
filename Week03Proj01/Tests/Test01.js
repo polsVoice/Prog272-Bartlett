d4e1c2fa15fd9c72e769145940dd13e839b30241
polsVoice <brent.bartlett1@gmail.com>
2014-01-22 05:26:39 -0800

    Added tests

---
+++ Week03Proj01/Tests/Test01.js
@@ -0,0 +1,9 @@
+describe("Elvenware Jasmine One Suite", function() {
+  it("expects 9 to be 9", function() {
+    expect(numGetter.getNine()).toBe(9);
+  });
+
+  it( "expects 8 to be 8", function(){
+	  expect( numGetter.getEight() ).toBe( 8 );
+  });
+});
