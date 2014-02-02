d4f91aa8f5dc0d4b9ac1e1292b68ae702bdd6074
polsVoice <brent.bartlett1@gmail.com>
2014-02-02 15:06:47 -0800

    updated unit tests

--- Cordova02-Bartlett/platforms/android/assets/www/js/test.js
+++ Cordova02-Bartlett/platforms/android/assets/www/js/test.js
@@ -1,11 +1,12 @@
+var testNum = 16;
 describe( "floofy nougat", function(){
 	it( "expects 16 degrees Fahrenheit to be -8.88 degrees Celsius", function(){
-		expect( converter.fToCelsiusConverter() ).toBeCloseTo( -8.88, 1 );
+		expect( converter.fToCelsius( testNum ) ).toBeCloseTo( -8.88, 1 );
 	});
 	it( "expects 16 miles to be 25.6 km", function(){
-		expect( converter.milesToKM() ).toBe( 25.6 );
+		expect( converter.milesToKM( testNum ) ).toBe( 25.6 );
 	});
 	it( "expects sqrt of 16 to be 4", function(){
-		expect( converter.squareRoot() ).toBe( 4 );
+		expect( converter.squareRoot( testNum ) ).toBe( 4 );
 	});
 });
