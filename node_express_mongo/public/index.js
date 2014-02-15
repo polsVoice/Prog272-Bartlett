06f4d2544a164b72de179e73592d9e9ad6870ee1
polsVoice <brent.bartlett1@gmail.com>
2014-02-15 14:14:21 -0800

    CSS and minor code e tweaks

--- node_express_mongo/public/index.js
+++ node_express_mongo/public/index.js
@@ -1,37 +1,18 @@
-/*function showData(){
-	$.getJSON( '/read', function( data ){
-		console.log( data );
-		for( var i = 0; i < data.length; i++ ){
-			$( "#mongoData" ).append( '<li>' + JSON.stringify( data[ i ] ) + '</li>' );
-		}
-	} );
-}
-
-$( document ).ready( function(){
-	showData();
-} );
-*/
-
 var dataDisplay = {
 	init: function(){
 		$( "#showButton" ).on( "click", dataDisplay.showRecord );
-		var field1 = $( "#line1" );
-		var field2 = $( "#line2" );
-		var field3 = $( "#line3" );
-		$.getJSON( '/read', function( data ){
-			theData = data[ 0 ];
-			field1.html( theData[ "firstName" ] + " " + theData[ "lastName" ] );
-			field2.html( theData[ "address" ] );
-			field3.html( theData[ "city" ] + " " + theData[ "state" ] + " " + theData[ "zip" ] );
-		} );
+		dataDisplay.populate( 0 );
 	},
 	showRecord: function(){
 		var value = $( "#userInput" ).val();
+		dataDisplay.populate( value );
+	},
+	populate: function( num ){
 		var field1 = $( "#line1" );
 		var field2 = $( "#line2" );
 		var field3 = $( "#line3" );
 		$.getJSON( '/read', function( data ){
-			theData = data[ value ];
+			theData = data[ num ];
 			field1.html( theData[ "firstName" ] + " " + theData[ "lastName" ] );
 			field2.html( theData[ "address" ] );
 			field3.html( theData[ "city" ] + " " + theData[ "state" ] + " " + theData[ "zip" ] );
