4ec6598e4ebdd2a9ae61b3af020b5fa4fa955eff
polsVoice <brent.bartlett1@gmail.com>
2014-02-15 01:24:52 -0800

    add paragraphs; data loads

--- node_express_mongo/public/index.js
+++ node_express_mongo/public/index.js
@@ -1,4 +1,4 @@
-function showData(){
+/*function showData(){
 	$.getJSON( '/read', function( data ){
 		console.log( data );
 		for( var i = 0; i < data.length; i++ ){
@@ -10,3 +10,23 @@ function showData(){
 $( document ).ready( function(){
 	showData();
 } );
+*/
+
+var dataDisplay = {
+	init: function(){
+		$( "#showButton" ).on( "click", dataDisplay.showRecord );
+	},
+	showRecord: function(){
+		var value = $( "#userInput" ).val();
+		var field1 = $( "#line1" );
+		var field2 = $( "#line2" );
+		var field3 = $( "#line3" );
+		$.getJSON( '/read', function( data ){
+			theData = data[ value ];
+			field1.html( theData[ "firstName" ] + " " + theData[ "lastName" ] );
+			field2.html( theData[ "address" ] );
+			field3.html( theData[ "city" ] + " " + theData[ "state" ] + " " + theData[ "zip" ] );
+		} );
+	}
+};
+dataDisplay.init();
