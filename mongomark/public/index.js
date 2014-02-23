fc137510aded0614d6c1dbe47564dfd4299de0d3
polsVoice <brent.bartlett1@gmail.com>
2014-02-23 00:54:32 -0800

    cleanup; moved all processing into one button

--- mongomark/public/index.js
+++ mongomark/public/index.js
@@ -1,27 +1,25 @@
 var mongoMark = {
 	init: function(){
 		$( "#inputData" ).on( "click", mongoMark.readMarkdown );
-		$( "#genHTML" ).on( "click", mongoMark.generateHTML );
-		$( "#printHTML" ).on( "click", mongoMark.printHTML );
 		$( "input:radio" ).on( "click", mongoMark.showSection );
 	},
 	readMarkdown: function(){
-		console.log( "readMarkdown button clicked" );
 		$.getJSON( "/insert", function( data ){
 			console.log( "In getJSON()" );
 			console.dir( data );
 		} );
-	},
-	generateHTML: function(){
+		$( "#message" ).html( "Read in markdown file." )
+	//},
+	//generateHTML: function(){
 		console.log( "generateHTML button clicked" );
 		$.get( "/generate" );
-		$( "#message" ).html( "HTML file has been generated." );
-	},
-	printHTML: function(){
+		$( "#message" ).append( "<br />HTML file has been generated." );
+	//},
+	//printHTML: function(){
 		$.get( "/read", function( data ){
 			console.log( "inside printHTML" );
 			$( "#output" ).html( data.result );
-			$( "#message" ).html( "Please make a selection below." );
+			$( "#message" ).append( "<br />Please make a selection below." );
 			} );
 	},
 	showSection: function(){
